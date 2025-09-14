af56cff672083e5d8d967b830a6c7ec6312979fb
Sanit <Sanitarov2004@gmail.com>
2025-09-14 18:27:15 +0700

    Initial Commit

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Lab_1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Alex",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
