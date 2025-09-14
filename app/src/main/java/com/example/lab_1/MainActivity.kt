4963c61eed3edf684989b7e586d03d5804dd23e4
Sanit <Sanitarov2004@gmail.com>
2025-09-14 18:50:34 +0700

    Compose

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Lab_1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Alex",
+                        name = "Piska",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Lab_1Theme {
-        Greeting("Android")
+        Greeting("Compose")
     }
 }
